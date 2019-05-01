62b6033d7d394a226ca793b9ec4bcd5799d3d738
Joy Lin <vvlnote@hotmail.com>
2019-05-01 21:04:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,11 @@ class App < Sinatra::Base
     post '/teams' do
 
       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
-      #params[:team][:members].each do |member|
-      #  member = Member.new(name: member[:name], power: member[:power], bio: member[:bio])
-       # member.team = @team
-       # @team.members << member
-      #end
+      params[:team][:members].each do |member|
+        member = Member.new(name: member[:name], power: member[:power], bio: member[:bio])
+        member.team = @team
+        @team.members << member
+      end
       puts "Name = #{@team.name}"
       erb :team
     end
